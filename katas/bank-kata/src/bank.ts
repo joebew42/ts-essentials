1685338b4d567e80bbe80a359011377ee05bf7b1
Joe Bew <joebew42@gmail.com>
2025-05-28 09:32:14 +0200

    refactor: extract functions for creating deposit and withdrawal

--- katas/bank-kata/src/bank.ts
+++ katas/bank-kata/src/bank.ts
@@ -14,20 +14,25 @@ type Bank = {
   printStatement: PrintStatementFunction;
 };
 
+function depositTransactionOf(amount: number) {
+  return { type: TransactionType.Deposit, amount }; // TODO: Move to a smart constructor
+}
+
+function withdrawalTransactionOf(amount: number) {
+  return { type: TransactionType.Withdrawal, amount }; // TODO: Move to a smart constructor
+}
+
 function bank(accounts: Map<string, Transaction[]>): Bank {
   return {
     // TODO: Extract Use Cases (or Application Services)
     deposit: function (accountId: AccountId, amount: DepositAmount): void {
       const transactions = accounts.get(accountId) || [];
-      const depositTransaction = { type: TransactionType.Deposit, amount }; // TODO: Add smart constructor?
+      const depositTransaction = depositTransactionOf(amount);
       accounts.set(accountId, [...transactions, depositTransaction]);
     },
     withdraw: function (accountId: AccountId, amount: WithdrawAmount): void {
       const transactions = accounts.get(accountId) || [];
-      const withdrawalTransaction = {
-        type: TransactionType.Withdrawal,
-        amount,
-      }; // TODO: Add smart constructor?
+      const withdrawalTransaction = withdrawalTransactionOf(amount);
       accounts.set(accountId, [...transactions, withdrawalTransaction]);
     },
     printStatement: function (accountId: AccountId): void {
